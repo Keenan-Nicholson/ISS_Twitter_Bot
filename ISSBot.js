d88d6fbacc5b183b50cc82c8055e6a41d7b4180b
Keenan Nicholson <keenan@pop-os.localdomain>
2022-09-15 12:47:35 -0230

    test launch

--- ISSBot.js
+++ ISSBot.js
@@ -9,7 +9,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const locs = process.env.LOCATIONS || "./locations.json";
+const locs = process.env.LOCATIONS || "locations.json";
 
 app.get("/", async (req, res) => {
   let html = `<h1>Hello From ISSBot!</h1>`;
